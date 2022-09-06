8f58ec53758ceabb8f6051e18c4ad22f63818fb1
raga <argadeva@gmail.com>
2022-09-06 10:22:42 +0700

    Bugfix HomePage Serveside Rendering & Add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
-import { ApolloProvider } from '@apollo/client';
+import { useRouter } from 'next/router';
 import ContextProvider from '@/contexts';
+import { ApolloProvider } from '@apollo/client';
 import Footer from '@/components/elements/Footer';
 import Navbar from '@/components/elements/Navbar';
 import apolloClient from '@/utils/apollo-client';
 import '@/public/assets/css/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const client = apolloClient();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      window.gtag('config', process.env.publicGAID, {
+        page_path: url,
+      });
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events]);
+
   return (
     <ContextProvider>
       <ApolloProvider client={client}>
