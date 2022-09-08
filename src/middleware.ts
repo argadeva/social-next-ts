97442c629d6fe30b341f09f8ce7dd5e6a14b81a9
raga <argadeva@gmail.com>
2022-09-08 10:38:45 +0700

    Bugfix https

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
   const currentEnv = process.env.NODE_ENV as Environment;
   if (currentEnv === 'production' && request.nextUrl.pathname.startsWith('/') && request.nextUrl.protocol !== 'https:') {
     return NextResponse.redirect(
-      `https://${request.headers.get('host')}${request.nextUrl.pathname}`, 301
+      `https://${request.nextUrl.host}`, 301
     );
   }
 }
